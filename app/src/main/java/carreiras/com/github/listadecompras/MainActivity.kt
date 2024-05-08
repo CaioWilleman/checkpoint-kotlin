e2a4c3b2d4490dc34ddf11706403beb8545c676e
CaioWilleman <114761554+CaioWilleman@users.noreply.github.com>
2024-05-08 19:59:25 -0300

    Update MainActivity.kt

--- app/src/main/java/carreiras/com/github/listadecompras/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/listadecompras/MainActivity.kt
@@ -10,19 +10,24 @@ import carreiras.com.github.listadecompras.adapter.ItemsAdapter
 import carreiras.com.github.listadecompras.model.ItemModel
 import carreiras.com.github.listadecompras.viewmodel.ItemsViewModel
 
+/** A classe MainActivity é a atividade principal da aplicação. Ela estende ComponentActivity, que é uma classe base para atividades*/
 class MainActivity : ComponentActivity() {
+    /** A variável viewModel é uma instância da classe ItemsViewModel. O ViewModel é usado para armazenar e gerenciar dados relacionados à UI de maneira consciente ao ciclo de vida.*/
     val viewModel: ItemsViewModel by viewModels()
 
+    /** Este método é chamado quando a atividade é criada. Ele contém a lógica de inicialização para a atividade.*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        /** O RecyclerView é um componente que exibe uma lista ou grade de itens. O ItemsAdapter é usado para fornecer as visualizações que representam os itens na lista.*/
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
         val itemsAdapter = ItemsAdapter()
 
         recyclerView.adapter = itemsAdapter
 
+        /**O botão e o EditText são usados para adicionar novos itens à lista. Quando o botão é clicado, o texto do EditText é adicionado à lista, desde que o EditText não esteja vazio.*/
         val button = findViewById<Button>(R.id.button)
         val editText = findViewById<EditText>(R.id.editText)
 
